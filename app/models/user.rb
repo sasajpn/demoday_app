0292dc619df6e3815b1e736f09073b2a1758ad59
kazuki <s.kazuki.japan@gmail.com>
2016-11-29 11:19:10 +0900

    user setting.devise.confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,18 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  user_name              :string(255)
+#  username               :string(255)
 #  email                  :string(255)      default(""), not null
 #  encrypted_password     :string(255)      default(""), not null
 #  birthday               :date
-#  given_name             :string(255)
-#  given_name_kana        :string(255)
-#  family_name            :string(255)
-#  family_namee_kana      :string(255)
 #  reset_password_token   :string(255)
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
 #  confirmation_token     :string(255)
 #  confirmed_at           :datetime
 #  confirmation_sent_at   :datetime
@@ -28,5 +27,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(username:  "test",
+                email: "test@test.com",
+                birthday: "2016-07-24",
+                password: "password",
+                password_confirmation: "password"
+                )
+user.skip_confirmation!
+user.save
