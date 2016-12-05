101eb47cfd39c6bea08234c65d1aa3312a896d2b
kazuki <s.kazuki.japan@gmail.com>
2016-12-06 00:21:33 +0900

    ほしいね！をするときの処置

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,6 +3,7 @@ class ChildrenController < ApplicationController
   before_action :no_book, only: [:create]
   before_action :become_deadline, only: [:new, :create]
   before_action :already_done, only: [:new, :create]
+  before_action :already_negotiate, only: [:create]
   before_action :set_parent, except: [:destroy]
 
   def new
@@ -55,6 +56,13 @@ class ChildrenController < ApplicationController
     @parent = Parent.find(params[:parent_id])
     if @parent.parent_child.present?
       redirect_to parents_url, notice: "その本の取引は終了しています。"
+    end
+  end
+
+  def already_negotiate
+    @book = Book.find(params[:child][:book_id])
+    if @book.parent.present? || @book.child.present?
+      redirect_to parents_url, notice: "その本はすでに取引に出しています"
     end
   end
 end
